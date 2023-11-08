6b95da7866e5d3eac4a9f1b6171614191186890f
achhansrun <akeragon907@gmail.com>
2023-11-08 14:02:34 -0900

    initialcommit

---
+++ Homework6/da6.hpp
@@ -0,0 +1,24 @@
+#include "llnode2.hpp"
+#include <memory>
+using std::unique_ptr;
+template<typename ValType>
+void reverseList(unique_ptr<LLNode2<ValType>>& head)
+{
+
+}
+
+class SlowMap
+{
+public:
+	SlowMap() 
+	{
+
+	}
+	~SlowMap()
+	{
+
+	}
+
+private:
+
+};
